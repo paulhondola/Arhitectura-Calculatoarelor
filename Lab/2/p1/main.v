e43bf5342a5fa1d85f01e33e23b730ac9323613a
Paul Hondola <paulhondola@gmail.com>
2024-10-08 19:25:08 +0300

    Lab 2 update

---
+++ Lab/2/p1/main.v
@@ -0,0 +1,22 @@
+module main (
+	input a, b, c,
+	output f1
+);
+	//write Verilog code here
+endmodule
+
+module main_tb;
+	reg a, b, c;
+	wire f1;
+
+	main main_i (.a(a), .b(b), .c(c), .f1(f1));
+
+	integer k;
+	initial begin
+		$display("Time\ta\tb\tc\tabcd_10\tf1");
+		$monitor("%0t\t%b\t%b\t%b\t%0d\t%b", $time, a, b, c, {a,b,c}, f1);
+		{a, b, c} = 0;
+		for (k = 1; k < 8; k = k + 1)
+			#10 {a, b, c} = k;
+	end
+endmodule
